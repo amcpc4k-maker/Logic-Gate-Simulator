1aef811a0d16b1dc4902defc43db6cc51d194106
amcpc4k-maker <amcpc4k@gmail.com>
2026-05-08 01:57:35 +0000

    Styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import './App.css'
 
 
 
-
 function App() {
   return (
-    <div style={{ backgroundColor: '#000', color: '#0f0', height: '100vh', padding: '20px', fontFamily: 'monospace' }}>
+    <div >
       <h1>LOGIC GATE SIMULATOR [V 1.0]</h1>
       <p> System initialized...</p>
       <p> Waiting for input toggles...</p>
