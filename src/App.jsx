ff242be9f31bbbea6c29dc748fd4e2f4c7325784
amcpc4k-maker <amcpc4k@gmail.com>
2026-05-08 02:49:51 +0000

    m

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ function App() {
       <h1> &#128009;LOGIC GATE SIMULATOR [V 1.0]</h1>
       </div>
       <p>Check Input Validation:</p>
-
+      <label>0-9:</label>
       <input type='checkbox'></input>
-
+      <label>T/F:</label>
       <input type='checkbox'></input>
-
+      <label>%:</label>
       <input type='checkbox'></input>
-
+      <label>00.00:</label>
       <input type='checkbox'></input><br></br>
       <textarea>eg. SET A = 1</textarea><br></br>
       <button>EXECUTE</button>
