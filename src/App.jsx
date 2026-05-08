09829e3277827884893d1961b399af882fcea219
amcpc4k-maker <amcpc4k@gmail.com>
2026-05-08 02:12:35 +0000

    m

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <h1>LOGIC GATE SIMULATOR [V 1.0]</h1>
       </div>
       <p>Check Input Validation:</p>
-      <textarea>eg. SET A = 1</textarea>
+      <textarea>eg. SET A = 1</textarea><br></br>
       <button>EXECUTE</button>
     </div>
   );
